f98c2297d14863f3ee8a1d3ce4a9826ec7287e08
Dawud Rehan <d.rehan7@gmail.com>
2020-12-24 00:51:43 -0500

    added logo and textarea and submit button. Need to make the quickRead() function work m8

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import logo from './flashread.png';
 
 function Header() {
     return (
         <header style={ headerStyle }>
             <span>FLASH_READ</span>
+            <span>
+                <img className="logo" src={logo} alt="Logo" />
+            </span>
         </header>
     );
 }
-
 const headerStyle = {
   backgroundColor: 'rgb(119, 222, 252)',
   padding: '5%',
@@ -17,6 +20,10 @@ const headerStyle = {
   borderTopRightRadius: '0%',
   fontSize: '50px',
   fontFamily: 'Georgia',
+  boxShadow: 'inset 2px 2px 2px 0px #ccc',
+//   -moz-box-shadow: 10px 10px 5px #ccc,
+//   -webkit-box-shadow: 10px 10px 5px #ccc,
+//   -khtml-box-shadow: 10px 10px 5px #ccc,
 
 }
 
